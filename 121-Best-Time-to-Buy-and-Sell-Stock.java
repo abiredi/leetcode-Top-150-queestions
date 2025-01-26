206ac06b505d89152418b38ab2c1993c308b3ec7
Abhi Ram Reddy <127978414+abiredi@users.noreply.github.com>
2025-01-25 22:00:04 -0600

    [121] [Time Beats: 99.84%] [Memory Beats: 40.03%] - LeetPush

--- 121-Best-Time-to-Buy-and-Sell-Stock.java
+++ 121-Best-Time-to-Buy-and-Sell-Stock.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-
-        int minprice = Integer.MAX_VALUE;
-        int maxprofit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < minprice)
-                minprice = prices[i];
-            else if (prices[i] - minprice > maxprofit)
-                maxprofit = prices[i] - minprice;
-        }
-        return maxprofit;
+         int maxProfit=0;
+         int minPrice = Integer.MAX_VALUE;
+         for(int i =0; i<prices.length;i++){
+            if(prices[i]<minPrice)
+            minPrice=prices[i];
+         else if(prices[i]-minPrice>maxProfit)
+         maxProfit= prices[i]-minPrice;
+    }
+    return maxProfit;
     }
 }
