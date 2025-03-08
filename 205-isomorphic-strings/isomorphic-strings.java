06388dcda874f6deab45bc4733a0a9a9ff81b330
Abhi Ram Reddy <127978414+abiredi@users.noreply.github.com>
2025-03-07 20:51:33 -0600

    Time: 19 ms (22.89%) | Memory: 45.5 MB (5.06%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,18 +1,38 @@
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         Map<Character, Character> sMap = new HashMap<>();
+//         Map<Character, Character> tMap = new HashMap<>();
+
+//         for(int i=0; i<s.length(); i++){
+//             if(!sMap.containsKey(s.charAt(i))){
+//                 if(tMap.containsKey(t.charAt(i))) return false;
+//                 sMap.put(s.charAt(i), t.charAt(i));
+//                 tMap.put(t.charAt(i), s.charAt(i));
+//             }
+
+//             if(!sMap.get(s.charAt(i)).equals(t.charAt(i))) return false;
+//         }
+
+//         return true;
+//     }
+// }
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> sMap = new HashMap<>();
-        Map<Character, Character> tMap = new HashMap<>();
+    private String transformString(String s){
+        Map<Character, Integer> indexMapping = new HashMap<>();
+        StringBuilder builder = new StringBuilder();
 
-        for(int i=0; i<s.length(); i++){
-            if(!sMap.containsKey(s.charAt(i))){
-                if(tMap.containsKey(t.charAt(i))) return false;
-                sMap.put(s.charAt(i), t.charAt(i));
-                tMap.put(t.charAt(i), s.charAt(i));
+        for(int i =0; i<s.length(); i++){
+            char c1 = s.charAt(i);
+
+            if(!indexMapping.containsKey(c1)){
+                indexMapping.put(c1,i);
             }
-
-            if(!sMap.get(s.charAt(i)).equals(t.charAt(i))) return false;
+            builder.append(Integer.toString(indexMapping.get(c1)));
+            builder.append(" ");
         }
-
-        return true;
+        return builder.toString();
+    }
+    public boolean isIsomorphic(String s, String t) {
+        return transformString(s).equals(transformString(t));
     }
 }
