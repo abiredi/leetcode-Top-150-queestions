b1932df4a24c9c29bb39b13cfcda1152fe2ac22f
Abhi Ram Reddy <127978414+abiredi@users.noreply.github.com>
2025-03-29 19:39:43 -0500

    [103] [Time Beats: 100.00%] [Memory Beats: 14.48%] - LeetPush

---
+++ Binary-Tree-Zigzag-Level-Order-Traversal.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+       List<List<Integer>> output = new ArrayList<>();
+        if(root == null) return output;
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean leftToRight = true;
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> levelNodes = new ArrayList<>();
+
+            for(int i=0;i<size;i++){
+                TreeNode node;
+
+                if(leftToRight){
+                    node = queue.pollFirst();
+                    if(node.left!=null) queue.addLast(node.left);
+                    if(node.right!=null) queue.addLast(node.right);
+                }else{
+                    node = queue.pollLast();
+                    if(node.right != null) queue.addFirst(node.right);
+                    if(node.left != null) queue.addFirst(node.left);
+                }
+                 levelNodes.add(node.val);
+
+            }
+             leftToRight = leftToRight == false ? true : false;
+            output.add(levelNodes);
+        }
+        return output;
+    }
+}
