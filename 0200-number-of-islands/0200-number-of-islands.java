5061f7dc6dd38e30eb15b6c124be86823725107b
Abhi Ram Reddy <127978414+abiredi@users.noreply.github.com>
2025-04-02 18:48:18 -0500

    Time: 8 ms (15.16%), Space: 48.8 MB (94.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -40,45 +40,43 @@
 //     }
 // }
 
-class Solution {
-    void dfs(char[][] grid, int r, int c) {
-        int nr = grid.length;
-        int nc = grid[0].length;
+// class Solution {
+//     void dfs(char[][] grid, int r, int c) {
+//         int nr = grid.length;
+//         int nc = grid[0].length;
 
-        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
-            return;
-        }
+//         if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+//             return;
+//         }
 
-        grid[r][c] = '0';
-        dfs(grid, r - 1, c);
-        dfs(grid, r + 1, c);
-        dfs(grid, r, c - 1);
-        dfs(grid, r, c + 1);
-    }
+//         grid[r][c] = '0';
+//         dfs(grid, r - 1, c);
+//         dfs(grid, r + 1, c);
+//         dfs(grid, r, c - 1);
+//         dfs(grid, r, c + 1);
+//     }
 
-    public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-
-        int nr = grid.length;
-        int nc = grid[0].length;
-        int num_islands = 0;
-        for (int r = 0; r < nr; ++r) {
-            for (int c = 0; c < nc; ++c) {
-                if (grid[r][c] == '1') {
-                    ++num_islands;
-                    dfs(grid, r, c);
-                }
-            }
-        }
-
-        return num_islands;
-    }
-}
+//     public int numIslands(char[][] grid) {
+//         if (grid == null || grid.length == 0) {
+//             return 0;
+//         }
+//         int nr = grid.length;
+//         int nc = grid[0].length;
+//         int num_islands = 0;
+//         for (int r = 0; r < nr; ++r) {
+//             for (int c = 0; c < nc; ++c) {
+//                 if (grid[r][c] == '1') {
+//                     ++num_islands;
+//                     dfs(grid, r, c);
+//                 }
+//             }
+//         }
+//         return num_islands;
+//     }
+// }
 
 // class Solution{
-//     public int dfs(char[][] grid, int r, int c){
+//      void dfs(char[][] grid, int r, int c){
 //         int newRow=grid.length;
 //         int newColumn=grid[0].length;
 
@@ -100,10 +98,10 @@ class Solution {
 //         int newRow=grid.length;
 //         int newColumn=grid[0].length;
 //         int num_islands=0;
-//         for(int r=0;r<newRow; newRow++){
-//             for(int c=0; c<newColumn; newColumn++){
+//         for(int r=0;r<newRow; ++newRow){
+//             for(int c=0; c<newColumn; ++newColumn){
 //                 if(grid[r][c]=='1'){
-//                     num_islands++;
+//                     ++num_islands;
 //                     dfs(grid,r,c);
 //                 }
 //             }
@@ -111,3 +109,49 @@ class Solution {
 //         return num_islands;
 //     }
 // }
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0'; // mark as visited
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.add(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            neighbors.add((row - 1) * nc + col);
+                            grid[row - 1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row + 1][col] == '1') {
+                            neighbors.add((row + 1) * nc + col);
+                            grid[row + 1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+                            neighbors.add(row * nc + col - 1);
+                            grid[row][col - 1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col + 1] == '1') {
+                            neighbors.add(row * nc + col + 1);
+                            grid[row][col + 1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+
+        return num_islands;
+    }
+}
