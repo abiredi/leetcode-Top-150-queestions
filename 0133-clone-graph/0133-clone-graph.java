0f6ffc98760621c44aa46a4fd7426b68eaae8b1c
Abhi Ram Reddy <127978414+abiredi@users.noreply.github.com>
2025-04-10 20:10:02 -0500

    Time: 28 ms (16.99%), Space: 42.9 MB (55.32%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -18,22 +18,52 @@ class Node {
 }
 */
 
-class Solution{
-    private HashMap<Node, Node> visited = new HashMap<>();
+// class Solution{
+//     private HashMap<Node, Node> visited = new HashMap<>();
+//     public Node cloneGraph(Node node) {
+//         if(node == null){
+//             return node;
+//         }
+
+//         if(visited.containsKey(node)) return visited.get(node);
+
+//         Node clonedNode = new Node(node.val, new ArrayList<>());
+//         visited.put(node, clonedNode);
+
+//         for(Node neighbour : node.neighbors){
+//             clonedNode.neighbors.add(cloneGraph(neighbour));
+//         }
+
+//         return clonedNode;
+//     }
+// }
+class Solution {
+    
     public Node cloneGraph(Node node) {
+        HashMap<Node, Node> visited = new HashMap<>();
         if(node == null){
             return node;
         }
 
-        if(visited.containsKey(node)) return visited.get(node);
+        Queue<Node> queue = new LinkedList<>();
 
-        Node clonedNode = new Node(node.val, new ArrayList<>());
-        visited.put(node, clonedNode);
+        queue.add(node);
+        visited.put(node, new Node(node.val, new ArrayList<>()));
 
-        for(Node neighbour : node.neighbors){
-            clonedNode.neighbors.add(cloneGraph(neighbour));
+
+        while(!queue.isEmpty()){
+            Node currentNode = queue.poll();
+
+            for(Node neighbor : currentNode.neighbors){
+                if(!visited.containsKey(neighbor)){
+                    visited.put(neighbor, new Node(neighbor.val, new ArrayList<>()));
+                    queue.add(neighbor);
+                }
+
+                visited.get(currentNode).neighbors.add(visited.get(neighbor));
+            }
         }
 
-        return clonedNode;
+        return visited.get(node);
     }
 }
