cd754537feb8b7bcd93259fac32e8fded9487407
Abhi Ram Reddy <127978414+abiredi@users.noreply.github.com>
2025-05-04 16:47:59 -0500

    Time: 0 ms (100%), Space: 40.4 MB (65.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,10 +1,25 @@
-
+public class Solution1 {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        //TC:-O(N);;SC:-o(1)
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
 
 class Solution{
     public int climbStairs(int n){
         if(n==1) return 1;
         if(n<0) return 0;
-
+//TC;o(n);sc:o(n)
         int[] dp = new int[n+1];
         dp[1]=1;
         dp[2]=2;
