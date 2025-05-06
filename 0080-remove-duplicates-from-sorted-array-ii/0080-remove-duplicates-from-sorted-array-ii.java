d6b4292f2ae89ae52137c54135571e4c39376b8f
Abhi Ram Reddy <127978414+abiredi@users.noreply.github.com>
2025-05-06 18:20:03 -0500

    Time: 0 ms (100%), Space: 46.9 MB (29.63%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,14 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-       if(nums.length==0){
-        return 0;
-       }
-       
-       int i=1;//pointer to iterate throughout the array
-       int j=1;//Pointer to track position for valid elements
-       int count =1;
+      if(nums.length == 0) return 0;
 
-       while(i<nums.length){
+      int i=1;
+      int j=1;
+      int count=1;
+
+      while(i<nums.length){
         if(nums[i]==nums[i-1]){
             count++;
             if(count>2){
@@ -18,11 +16,10 @@ class Solution {
         }else{
             count=1;
         }
-        nums[j] = nums[i];
-          
-            i++;  
-            j++;
-       }
-        return j;
+        nums[j]=nums[i];
+        j++;
+        i++;
+      }
+      return j;
     }
 }
