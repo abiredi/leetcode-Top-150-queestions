678f72db782a632f6192e3a1b7f5180ffca6d7a9
Abhi Ram Reddy <127978414+abiredi@users.noreply.github.com>
2025-05-08 19:54:26 -0500

    Time: 6 ms (85.41%), Space: 43 MB (79.87%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,15 +1,31 @@
-class Solution {
-    public String reverseWords(String s) {
-        //lets trim the white spaces
+// class Solution {
+//     public String reverseWords(String s) {
+//         //lets trim the white spaces
+//         s=s.trim();
+//         String[] strings = s.split("\\s+");
+//         StringBuilder output = new StringBuilder();
+
+//         for(int i= strings.length-1;i>=0;i--){
+//             output.append(" ");
+//             output.append(strings[i]);
+//         }
+//         return output.toString().trim();
+        
+//     }
+// }
+
+class Solution{
+    public String reverseWords(String s){
+
         s=s.trim();
         String[] strings = s.split("\\s+");
         StringBuilder output = new StringBuilder();
 
-        for(int i= strings.length-1;i>=0;i--){
+        for(int i=strings.length-1;i>=0;i--){
             output.append(" ");
             output.append(strings[i]);
         }
+
         return output.toString().trim();
-        
     }
 }
